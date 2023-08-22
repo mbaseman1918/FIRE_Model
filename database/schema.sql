4fb5003ca1ffec4c0ef817a896cb67e28cf8eec5
Matthew Baseman <matthew.baseman@gmail.com>
2023-08-22 15:31:56 -0700

    Building out skeleton structure for app

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  firstName TEXT,
+  lastName TEXT
+)
---
+++ server/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const controllers = require("./controllers/firecontrollers.js");
+// const logger = require("./middleware/logger.js");
+// const authChecker = require("./middleware/authChecker.js");
+
+const app = express();
+
+// TODO: Add app-wide middleware
+
+app.use(express.static('client/dist'));
+app.use(express.json());
+app.use(express.urlencoded());
+// TODO: Set up static service of assets
+
+// TODO: Define routes
+app.post('/users', controllers.addUser)
+app.get('/users', controllers.getUser)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log(`Server listening at http://localhost:${PORT}`);
